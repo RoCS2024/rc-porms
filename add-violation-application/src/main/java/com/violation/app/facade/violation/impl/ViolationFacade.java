2aa9da9b6f235a1c524f941c9ba6f5a95e448bb7
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-03-21 22:58:04 +0800

    RCPS-73: Code Cleanup for Prefect System

--- add-violation-application/src/main/java/com/violation/app/facade/violation/impl/ViolationFacade.java
+++
@@ -1,9 +0,0 @@
-package com.violation.app.facade.violation.impl;
-
-import com.violation.app.model.item.Violation;
-
-import java.util.List;
-
-public interface ViolationFacade {
-    void addViolation(String violation, String type, int commServHours);
-}

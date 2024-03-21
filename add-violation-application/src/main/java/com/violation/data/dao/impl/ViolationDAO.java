22cec644dbc0607951111979f2e882e9e68cefc5
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-03-21 08:35:16 +0800

    RCPS-73: Code Cleanup for Prefect System

--- add-violation-application/src/main/java/com/violation/data/dao/impl/ViolationDAO.java
+++
@@ -1,7 +0,0 @@
-package com.violation.data.dao.impl;
-
-import com.violation.app.model.item.Violation;
-
-public interface ViolationDAO {
-    void addViolation(com.violation.app.model.item.Violation violation);
-}

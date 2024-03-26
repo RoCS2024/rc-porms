884ecf488916dd16cf22d1beedfd59cdefdf9e6d
harold dinglasan <harolddinglasan90@gmail.com>
2024-03-26 11:49:52 +0800

    Updated Prefect Office Record Management

---
+++ src/test/java/com/prefect/office/record/management/app/facade/prefect/violation/impl/ViolationFacadeImplTest.java
@@ -0,0 +1,7 @@
+package com.prefect.office.record.management.app.facade.prefect.violation.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ViolationFacadeImplTest {
+
+}
---
+++ src/test/java/com/prefect/office/record/management/data/dao/prefect/communityservice/impl/CommunityServiceDaoImplTest.java
@@ -0,0 +1,7 @@
+package com.prefect.office.record.management.data.dao.prefect.communityservice.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommunityServiceDaoImplTest {
+
+}

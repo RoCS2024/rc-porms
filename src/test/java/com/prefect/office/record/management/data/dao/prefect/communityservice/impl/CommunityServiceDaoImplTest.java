36412b1952056b227cce23dab3ce4795174ccc90
katieann7 <150576208+katieann7@users.noreply.github.com>
2024-03-27 22:00:34 +0800

    RCPS-119: Prefect System: Create ViolationDaoImplTest unit test (#36)
    * RCPS-117: Prefect System: Create CommunityServiceDaoImplTest unit test
    
    * RCPS-118: Prefect System: Create OffenseDaoImplTest unit test
    
    * RCPS-119: Prefect System: Create ViolationDaoImplTest unit test
    
    * RCPS-119: Prefect System: Create ViolationDaoImplTest unit test
    
    ---------
    
    Co-authored-by: Kate Ann I. Amulong <amulongkateann@gmail.com>

--- src/test/java/com/prefect/office/record/management/data/dao/prefect/communityservice/impl/CommunityServiceDaoImplTest.java
+++ src/test/java/com/prefect/office/record/management/data/dao/prefect/communityservice/impl/CommunityServiceDaoImplTest.java
@@ -1,7 +1,64 @@
 package com.prefect.office.record.management.data.dao.prefect.communityservice.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.prefect.office.record.management.appl.model.communityservice.CommunityService;
+import com.prefect.office.record.management.data.dao.prefect.communityservice.CommunityServiceDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 
 class CommunityServiceDaoImplTest {
+    private CommunityServiceDao communityServiceDao;
+    private List<CommunityService> communityServices;
 
+    @BeforeEach
+    public void setUp() {
+        communityServices = new ArrayList<>();
+        CommunityService communityService1 = new CommunityService();
+        CommunityService communityService2 = new CommunityService();
+        communityServices.add(communityService1);
+        communityServices.add(communityService2);
+
+        communityServiceDao = mock(CommunityServiceDao.class);
+    }
+
+    @Test
+    public void testGetAllCs() {
+        when(communityServiceDao.getAllCs()).thenReturn(communityServices);
+        List<CommunityService> communityServiceList = communityServiceDao.getAllCs();
+        assertEquals(communityServiceList.size(), 2);
+    }
+
+    @Test
+    public void testGetCsById() {
+        CommunityService communityService1 = new CommunityService();
+        communityService1.setId(1);
+
+        when(communityServiceDao.getCsById(1)).thenReturn(communityService1);
+
+        CommunityService expectedCommunityService = communityServiceDao.getCsById(1);
+
+        assertEquals(expectedCommunityService, communityService1);
+        assertEquals(expectedCommunityService.getId(), communityService1.getId());
+    }
+    @Test
+    public void testRenderCs() {
+        CommunityService communityService = new CommunityService();
+        communityService.setId(1);
+
+        try {
+            when(communityServiceDao.renderCs(communityService)).thenReturn(true);
+            when(communityServiceDao.getCsById(1)).thenReturn(communityService);
+
+            CommunityService expectedCommunityService = communityServiceDao.getCsById(1);
+            assertEquals(expectedCommunityService.getId(), communityService.getId());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/prefect/office/record/management/data/dao/prefect/offense/impl/OffenseDaoImplTest.java
+++ src/test/java/com/prefect/office/record/management/data/dao/prefect/offense/impl/OffenseDaoImplTest.java
@@ -1,7 +1,72 @@
 package com.prefect.office.record.management.data.dao.prefect.offense.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.prefect.office.record.management.appl.model.offense.Offense;
+import com.prefect.office.record.management.data.dao.prefect.offense.OffenseDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class OffenseDaoImplTest {
+    private OffenseDao offenseDao;
+    private List<Offense> offenses;
 
+    @BeforeEach
+    public void setUp() {
+        offenses = new ArrayList<>();
+        Offense offense1 = new Offense();
+        Offense offense2 = new Offense();
+        offenses.add(offense1);
+        offenses.add(offense2);
+
+        offenseDao = mock(OffenseDao.class);
+    }
+    @Test
+    public void testGetAllOffenses() {
+        when(offenseDao.getAllOffenses()).thenReturn(offenses);
+        List<Offense> offenseList = offenseDao.getAllOffenses();
+        assertEquals(offenseList.size(), 2);
+    }
+
+    @Test
+    public void testGetOffenseById() {
+        Offense offense1 = new Offense();
+        offense1.setId(1);
+
+        when(offenseDao.getOffenseByID(1)).thenReturn(offense1);
+
+        Offense expectedOffense = offenseDao.getOffenseByID(1);
+
+        assertEquals(expectedOffense, offense1);
+        assertEquals(expectedOffense.getId(), offense1.getId());
+    }
+
+    @Test
+    public void testAddOffense() {
+        Offense offense = new Offense();
+        offense.setId(1);
+
+        when(offenseDao.addOffense(offense)).thenReturn(true);
+        when(offenseDao.getOffenseByID(1)).thenReturn(offense);
+
+        Offense expectedOffense = offenseDao.getOffenseByID(1);
+        assertEquals(expectedOffense.getId(), offense.getId());
+    }
+    @Test
+    public void testUpdateOffense() {
+        Offense offense = new Offense();
+        offense.setId(1);
+
+        when(offenseDao.addOffense(offense)).thenReturn(true);
+        when(offenseDao.updateOffense(offense)).thenReturn(true);
+        when(offenseDao.getOffenseByID(1)).thenReturn(offense);
+
+        Offense expectedOffense = offenseDao.getOffenseByID(1);
+        assertEquals(expectedOffense.getId(), offense.getId());
+    }
 }

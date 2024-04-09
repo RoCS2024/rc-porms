c149c2c878ca672266b9010c818261405771d166
unknown <julybayot26>
2024-04-09 15:41:59 +0800

    RCPS-115 Prefect System: Create Documentation for Violation Model

--- src/main/java/com/prefect/office/record/management/appl/model/violation/Violation.java
+++ src/main/java/com/prefect/office/record/management/appl/model/violation/Violation.java
@@ -1,5 +1,11 @@
+/**
+ * this is the Violation class where the fields, constructors, getter and setter resides.
+ */
 package com.prefect.office.record.management.appl.model.violation;
 
+/**
+ * This is Violation model.
+ */
 public class Violation {
     private int id;
     private String violation;
@@ -8,13 +14,18 @@ public class Violation {
 
     public Violation() {
     }
-
+    /**
+     * This is the Violation fields.
+     */
     public Violation(String violation, String type, int commServHours) {
         this.violation = violation;
         this.type = type;
         this.commServHours = commServHours;
     }
 
+    /**
+     * This is the Violation Getter and Setter.
+     */
     public void setId(int id) {
         this.id = id;
     }

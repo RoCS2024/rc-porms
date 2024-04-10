66b63c5350e83f4e72f814e2e13d93ab7297c1ab
unknown <vincesarmiento63@gmail.com>
2024-04-10 10:34:50 +0800

    RCPS-116 Prefect System: Create Documentation for CommunityServiceDao and CommunityServiceDaoImpl

--- src/main/java/com/prefect/office/record/management/data/dao/prefect/communityservice/CommunityServiceDao.java
+++ src/main/java/com/prefect/office/record/management/data/dao/prefect/communityservice/CommunityServiceDao.java
@@ -5,8 +5,32 @@ import com.prefect.office.record.management.appl.model.communityservice.Communit
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Interface for accessing and managing community service records in the database.
+ */
 public interface CommunityServiceDao {
+
+    /**
+     * Retrieves all community service records from the database.
+     *
+     * @return A list of all community service records.
+     */
     List<CommunityService> getAllCs();
+
+    /**
+     * Retrieves a specific community service record by its ID from the database.
+     *
+     * @param id The ID of the community service record to retrieve.
+     * @return The community service record corresponding to the given ID.
+     */
     CommunityService getCsById(int id);
+
+    /**
+     * Records a new community service entry into the database.
+     *
+     * @param cs The community service record to insert.
+     * @return True if the insertion is successful, false otherwise.
+     * @throws SQLException If an SQL exception occurs during the insertion process.
+     */
     boolean renderCs(CommunityService cs) throws SQLException;
 }

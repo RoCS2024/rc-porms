65b3db8f4eb07c69bb54cb4a68e0e77ba83c9485
daiahnadine <157860737+daiahnadine@users.noreply.github.com>
2024-04-10 14:42:07 +0800

    RCPS-117 Prefect System: Create Documentation for OffenseDao and OffenseDaoImpl

--- src/main/java/com/prefect/office/record/management/data/dao/prefect/offense/OffenseDao.java
+++ src/main/java/com/prefect/office/record/management/data/dao/prefect/offense/OffenseDao.java
@@ -3,11 +3,34 @@ package com.prefect.office.record.management.data.dao.prefect.offense;
 import com.prefect.office.record.management.appl.model.offense.Offense;
 
 import java.util.List;
-
+/**
+ * This is the interface for the OffenseDao
+ */
 public interface OffenseDao {
+    /**
+     * This retrieves all Offenses from the database
+     * @return list of all the offenses
+     */
     List<Offense> getAllOffenses();
+
+    /**
+     * This gets an Offense from the database with a specific ID
+     * @param id is the id of the Offense
+     * @return the Offense with specific ID
+     */
     Offense getOffenseByID (int id);
+
+    /**
+     * This updates an Offense in the database
+     * @param offense is the Offense to update
+     * @return true if update of Offense is successful
+     */
     boolean updateOffense (Offense offense);
 
+    /**
+     * This adds an Offense in the database
+     * @param offense is the Offense to add
+     * @return true if adding of Offense is successful
+     */
     boolean addOffense(Offense offense);
 }
